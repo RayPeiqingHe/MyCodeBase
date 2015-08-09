99abb8a5461b12344af4ecb77eec5b3b3556c617
Rayhe <ray.peiqing.he@gmail.com>
2015-08-09 14:56:36 -0400

    Added Historical Quote loader

--- CSharpCodeBase/YahooFinanceDownloader/YahooFinanceDownloader/YahooHistoricalLoader.cs
+++ CSharpCodeBase/YahooFinanceDownloader/YahooFinanceDownloader/YahooHistoricalLoader.cs
@@ -31,7 +31,12 @@ File format:
 http://ichart.yahoo.com/table.csv?s=MSFT&a=0&b=1&c=2000&d=11&e=24&f=2014&g=w&ignore=.csv
  
  * Example: http://ichart.finance.yahoo.com/table.csv?s=IBM&a=1&b=25&c=2009&d=11&e=13&f=2010&g=d&ignore=.csv
-*/
+
+ Get Dividend only data
+ * 
+ * http://ichart.finance.yahoo.com/x?s=IBM&a=00&b=2&c=1962&d=04&e=25&f=2011&g=v&y=0&z=30000
+ * 
+ */
 
 namespace YahooFinanceDownloader
 {
@@ -83,48 +88,78 @@ namespace YahooFinanceDownloader
 
             //string url = string.Format("http://ichart.finance.yahoo.com/table.csv?s={0}&c={1}", ticker, yearToStartFrom);
 
-            string url = string.Format("http://ichart.finance.yahoo.com/table.csv?s={0}&a={1}&b={2}&c={3}&d={4}&e={5}&f={6}&g=d&ignore=.csv", ticker,
+            string url = string.Format("http://ichart.finance.yahoo.com/table.csv?s={0}&a={1}&b={2}&c={3}&d={4}&e={5}&f={6}&g={7}&ignore=.csv", ticker,
                 startMonth, startDay, startYear, endMonth, endDay, endYear, freqStr);
 
             return url;
         }
 
+        private static string getUrlStringAll(string ticker, string fields)
+        {
+            string url = string.Format("http://ichart.finance.yahoo.com/table.csv?s={0}&a=0&b=1&c=2015&d=7&e=7&f={1}&g=d&ignore=.csv", ticker, fields);
+
+            return url;
+        }
+
+        public static List<HistoricalStock> DownloadDataAll(string ticker, string fields, out string header)
+        {
+            List<HistoricalStock> retval = null;
+
+            using (WebClient web = new WebClient())
+            {
+                string data = web.DownloadString(getUrlStringAll(ticker, fields));
+
+                retval = GetSecurityObjFromURL(data, ticker, out header);
+            }
+
+            return retval;
+        }
+
         public static List<HistoricalStock> DownloadData(string ticker, DateTime startDate, DateTime endDate, out string header, frequency freq = frequency.daily)
         {
-            List<HistoricalStock> retval = new List<HistoricalStock>();
+            List<HistoricalStock> retval = null;
 
             using (WebClient web = new WebClient())
             {
                 string data = web.DownloadString(getUrlString(ticker, startDate, endDate, freq));
 
-                data = data.Replace("\r", "");
-
-                string[] rows = data.Split(new string[]{"\n"}, StringSplitOptions.None);
-
-                header = rows[0];
-
-                //First row is headers so Ignore it
-                for (int i = 1; i < rows.Length; i++)
-                {
-                    if (rows[i].Replace("n", "").Trim() == "") continue;
-
-                    string[] cols = rows[i].Split(',');
-
-                    HistoricalStock hs = new HistoricalStock();
-                    hs.ticker = ticker;
-                    hs.Date = Convert.ToDateTime(cols[0]);
-                    hs.Open = Convert.ToDouble(cols[1]);
-                    hs.High = Convert.ToDouble(cols[2]);
-                    hs.Low = Convert.ToDouble(cols[3]);
-                    hs.Close = Convert.ToDouble(cols[4]);
-                    hs.Volume = Convert.ToDouble(cols[5]);
-                    hs.AdjClose = Convert.ToDouble(cols[6]);
-
-                    retval.Add(hs);
-                }
-
-                return retval;
+                retval = GetSecurityObjFromURL(data, ticker, out header);
             }
+
+            return retval;
+        }
+
+        private static List<HistoricalStock> GetSecurityObjFromURL(string data, string ticker, out string header)
+        {
+            List<HistoricalStock> retval = new List<HistoricalStock>();
+
+            data = data.Replace("\r", "");
+
+            string[] rows = data.Split(new string[] { "\n" }, StringSplitOptions.None);
+
+            header = rows[0];
+
+            //First row is headers so Ignore it
+            for (int i = 1; i < rows.Length; i++)
+            {
+                if (rows[i].Replace("n", "").Trim() == "") continue;
+
+                string[] cols = rows[i].Split(',');
+
+                HistoricalStock hs = new HistoricalStock();
+                hs.ticker = ticker;
+                hs.Date = Convert.ToDateTime(cols[0]);
+                hs.Open = Convert.ToDouble(cols[1]);
+                hs.High = Convert.ToDouble(cols[2]);
+                hs.Low = Convert.ToDouble(cols[3]);
+                hs.Close = Convert.ToDouble(cols[4]);
+                hs.Volume = Convert.ToDouble(cols[5]);
+                hs.AdjClose = Convert.ToDouble(cols[6]);
+
+                retval.Add(hs);
+            }
+
+            return retval;
         }
     }
 }
--- CSharpCodeBase/YahooFinanceDownloader/YahooPriceBatchLoader/Program.cs
+++ CSharpCodeBase/YahooFinanceDownloader/YahooPriceBatchLoader/Program.cs
@@ -3,13 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using YahooFinanceDownloader;
+using System.Configuration;
+using System.Data;
+using SQLLib;
+using log4net;
+using log4net.Config;
 
 namespace YahooPriceBatchLoader
 {
     class Program
     {
+        private static readonly ILog logger = 
+          LogManager.GetLogger(typeof(Program));
+    
+        static Program()
+        {
+            XmlConfigurator.Configure();
+        }
+
         static void Main(string[] args)
         {
+            string connstr = ConfigurationManager.AppSettings["SQLConnection"];
+
+            string stagingTable = ConfigurationManager.AppSettings["StagingTableName"];
+
+            string sp_OHLC = ConfigurationManager.AppSettings["InsertOHLC"];
+
+            using (SQLUtils sql = new SQLUtils(connstr))
+            {
+                try
+                {
+                    string query = ConfigurationManager.AppSettings["SelectSerityMaster"];
+
+                    DataTable dtSecurities = new DataTable();
+
+                    sql.GetTableFromQuery(query, dtSecurities);
+
+                    string headers = string.Empty;
+
+                    foreach (DataRow row in dtSecurities.Rows)
+                    {
+                        string ticker = row["Ticker"].ToString();
+
+                        if (row["InsertDate"] == DBNull.Value)
+                        {
+                            List<HistoricalStock> retval = HistoricalStockDownloader.DownloadData(ticker, new DateTime(2015, 1, 1),
+                                new DateTime(2015, 8, 7), out headers);
+
+                            sql.BulkInsert<HistoricalStock>(stagingTable, retval);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex.Message);
+                }
+            }
         }
     }
 }
