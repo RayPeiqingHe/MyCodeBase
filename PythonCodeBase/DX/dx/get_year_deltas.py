f5632d67858fe58142249818d87144526c0a9664
RayPeiqingHe <ray.peiqing.he@gmail.com>
2015-12-28 20:12:21 -0500

    Derivative valuation library
    1. random number generation with variance reduction
    2. constant short rate class
    3. simulation of geometric brownian motion
    4. simulation of jump diffusion process
    5. simulation of square root process

---
+++ PythonCodeBase/DX/dx/get_year_deltas.py
@@ -0,0 +1,34 @@
+__author__ = 'Ray'
+
+#
+# DX Library Frame
+# get_year_deltas.py
+#
+
+import numpy as np
+
+
+def get_year_deltas(date_list, day_count=365.):
+    ''' Return vector of floats with day deltas in years
+    Initial value normalized to zero
+
+    Parameters
+    ==========
+    date_list : list of array
+        collection of datetime objects
+    day_count : float
+        number of days for a year
+        (to account for different convention)
+
+    Results
+    =======
+    delta_list : array
+        year fractions
+    '''
+
+
+    start = date_list[0]
+
+    delta_list = [(date - start).days / day_count for date in date_list]
+
+    return np.array(delta_list)
